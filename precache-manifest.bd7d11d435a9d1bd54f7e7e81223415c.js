113d14c8c369d15a1319446d782cbe5061b4f6e2
warlock-hash <utcsm44@gmail.com>
2019-10-11 02:25:08 +0500

    Updates

---
+++ precache-manifest.bd7d11d435a9d1bd54f7e7e81223415c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7dc4cbff704a12b28521336d4012b7d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8342b3bb92f302ec3ae6",
+    "url": "/monsters-rolodex/static/css/main.6c7bf76f.chunk.css"
+  },
+  {
+    "revision": "a7ffd671cb21d8ff7fb4",
+    "url": "/monsters-rolodex/static/js/2.fe5198a4.chunk.js"
+  },
+  {
+    "revision": "8342b3bb92f302ec3ae6",
+    "url": "/monsters-rolodex/static/js/main.7eda7f72.chunk.js"
+  },
+  {
+    "revision": "d78c7e54dca26e3c25e2",
+    "url": "/monsters-rolodex/static/js/runtime-main.aaf9d0bd.js"
+  }
+]);
